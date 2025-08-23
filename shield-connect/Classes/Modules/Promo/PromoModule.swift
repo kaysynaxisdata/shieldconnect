86a99b45bece386c1b82dd84ea0220d0b0e4723c
a.bugaev <a.bugaev@growire.org>
2025-08-23 20:45:04 +0600

    bug fixes

---
+++ shield-connect/Classes/Modules/Promo/PromoModule.swift
@@ -0,0 +1,8 @@
+//
+//  PromoModule.swift
+//  shield-connect
+//
+//  Created by Александр on 22.08.2025.
+//
+
+import Foundation
---
+++ shield-connect/Classes/Modules/Promo/PromoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PromoViewModel.swift
+//  shield-connect
+//
+//  Created by Александр on 22.08.2025.
+//
+
+import Foundation
